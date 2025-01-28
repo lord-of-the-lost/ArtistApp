657c98d4a49ca6452ff657468008521e994207e0
lord-of-the-lost <ignatov.99@yandex.ru>
2025-01-28 18:35:39 +0300

    Подготовил проект

--- ArtApp/ArtistListViewController.swift
+++ ArtApp/ArtistListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ArtistListViewController.swift
 //  ArtApp
 //
 //  Created by Николай Игнатов on 28.01.2025.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ArtistListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
